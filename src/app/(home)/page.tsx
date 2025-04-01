fa3ef21cbd40cdc899ebddb3c8c435e7f39a8034
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-04-01 17:18:21 -0300

    Next structure: #0004;

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
+import { SettingsTabs } from './SettingsTabs'
+import { Textarea } from '@/components/Form/Textarea'
+
+export default function Home() {
+    return (
+        <>
+            <div>Hello World</div>
+        </>
+    )
+}
